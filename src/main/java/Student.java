0460d3f0783d8c8d46ad7ae6bb1caec3399e421e
Piotr Hankiewicz <piotr.hankiewicz@gig.com>
2020-05-31 21:24:08 +0200

    feature: display student's birthday as formatted string instead of timestamp

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
 import dev.morphia.annotations.Transient;
@@ -34,6 +35,8 @@ public class Student {
     private int index;
     private String firstName;
     private String lastName;
+    @JsonFormat(shape=JsonFormat.Shape.STRING,
+            pattern="yyyy-MM-dd", timezone="CET")
     private Date birthday;
 
     public Student(){
